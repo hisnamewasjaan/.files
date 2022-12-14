a7940adbea24e2f5bcae23d1ffc6a4b486f59a8f
Jan Bentzen <hisnamewasjaan@gmail.com>
2022-12-14 21:22:14 +0100

    macos default update, show all files

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -147,13 +147,16 @@ echo "  › Disable the warning before emptying the Trash"
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
 echo "  › Save to disk by default, instead of iCloud"
-defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+#defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
 echo "  › Display full POSIX path as Finder window title"
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
 echo "  › Disable the warning when changing a file extension"
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+echo "  › Show Hidden Files & Folders"
+defaults write com.apple.finder AppleShowAllFiles -boolean true
 
 #############################
 
