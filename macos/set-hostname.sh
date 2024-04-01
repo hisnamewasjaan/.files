9c0ba80d29aa434ae141b650d8599aa58c977fa7
Jan Bentzen <hisnamewasjaan@gmail.com>
2024-04-01 10:09:33 +0200

    improve script to set computername, hostname, localhostname and netbiosname

--- macos/set-hostname.sh
+++ macos/set-hostname.sh
@@ -5,12 +5,14 @@ echo 'Current LocalHostName <'$(sudo scutil --get LocalHostName)'>'
 echo 'Current NetBIOSName <'$(sudo defaults read /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName)'>'
 
 vared -p 'hostname? ' -c newhostname
+vared -p 'localhostname? ' -c newlocalhostname
 
 echo 'Setting hostname <'$newhostname'>'
+echo 'Setting localhostname <'$newlocalhostname'>'
 
 sudo scutil --set ComputerName $newhostname && \
 sudo scutil --set HostName $newhostname && \
-sudo scutil --set LocalHostName $newhostname && \
+sudo scutil --set LocalHostName $newlocalhostname && \
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $newhostname
 
 dscacheutil -flushcache
