089c7b338095989040a27c6a4ef32730c776f0b6
Jan Bentzen <hisnamewasjaan@gmail.com>
2024-11-03 19:56:21 +0100

    updated function to apply ERST coding standard

--- functions/erst.functions.sh
+++ functions/erst.functions.sh
@@ -7,9 +7,9 @@ kodestandard() {
     echo "$tmpdir" &&
     ([[ -n "$@" ]] &&
     # clone specific branch if specified in arguments:
-    git clone git@rep.erst.dk:sector9/sector9-kodestandard.git "$tmpdir" --depth=1 --quiet --branch "$@" ||
+    git clone git@rep.erst.dk:kerneb/kerneb-kodestandard.git "$tmpdir" --depth=1 --quiet --branch "$@" ||
     # otherwise clone default branch:
-    git clone git@rep.erst.dk:sector9/sector9-kodestandard.git "$tmpdir" --depth=1 --quiet) &&
+    git clone git@rep.erst.dk:kerneb/kerneb-kodestandard.git "$tmpdir" --depth=1 --quiet) &&
     cp -r "$tmpdir/gradle-templates/"* gradle &&
     cp "$tmpdir/.editorconfig" .
     cp "$tmpdir/sonar-project.properties" .
